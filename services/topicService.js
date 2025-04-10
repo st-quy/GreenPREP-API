ca407ded34b34feb2496b01b3db2a74afa99c7a1
Sinh Dang <sinh.dang@stunited.vn>
2025-04-10 16:06:04 +0700

    feat(topic): add getAllTopics service

---
+++ services/topicService.js
@@ -0,0 +1,18 @@
+const { Topic, Part, Question } = require("../models");
+
+const getAllTopics = async () => {
+  try {
+    const topics = await Topic.findAll();
+    return {
+      status: 200,
+      message: "Get all topic successfully",
+      data: topics,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  getAllTopics,
+};
