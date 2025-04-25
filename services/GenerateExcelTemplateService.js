be7656bf288270f47b94bd857eb87a4e72130b53
Sinh Dang <sinh.dang@stunited.vn>
2025-04-25 13:32:31 +0700

    feat: import exams

--- services/GenerateExcelTemplateService.js
+++ services/GenerateExcelTemplateService.js
@@ -15,45 +15,38 @@ const parseExcelBuffer = async (buffer) => {
 
   const result = [];
 
-  // Đọc từng dòng dữ liệu
   sheet.eachRow((row, rowIndex) => {
-    if (rowIndex === 1) return; // Bỏ qua header
-    const part = row.getCell(1).value;
-    const skill = row.getCell(2).value;
+    if (rowIndex === 1) return; 
+    const part = row.getCell(2).value;
+    const skill = row.getCell(1).value;
 
     if (part && skill) {
-      // Xử lý nếu part là số, và skill là chuỗi
       const cleanPart = isNaN(part) ? part.toString().trim() : `Part${part}`;
       const cleanSkill = skill.toString().trim();
       result.push({ part: cleanPart, skill: cleanSkill });
     }
   });
 
-  // Group by part và gom lại các skill tương ứng vào một mảng duy nhất
   const grouped = result.reduce((acc, { part, skill }) => {
     if (!acc[skill]) acc[skill] = { part: [] };
     acc[skill].part.push(part);
     return acc;
   }, {});
 
-  // Tạo kết quả cuối cùng
   const finalResult = {
-    success: true,
-    data: {
-      part: [], // mảng part trống
-      skill: [], // mảng skill trống
-    },
+    part: [], 
+    skillName: [], 
   };
 
-  // Lấy tất cả 'skill' và 'part' từ grouped
   Object.keys(grouped).forEach((skill) => {
-    finalResult.data.skill.push(skill); // Thêm skill vào mảng skill
-    finalResult.data.part.push(...grouped[skill].part); // Thêm tất cả part vào mảng part
+    finalResult.skillName.push(skill);
+    finalResult.part.push(...grouped[skill].part); 
   });
 
-  // Loại bỏ trùng lặp trong phần "part" và sắp xếp lại theo thứ tự nếu cần
-  finalResult.data.part = [...new Set(finalResult.data.part)]; // Loại bỏ các giá trị trùng lặp trong mảng part
-  finalResult.data.skill = [...new Set(finalResult.data.skill)]; // Loại bỏ các giá trị trùng lặp trong mảng skill
+  finalResult.part = [...new Set(finalResult.part)]; 
+  finalResult.skillName = [...new Set(finalResult.skillName)]; 
+
+  
 
   return finalResult;
 };
