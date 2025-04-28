904d4b49d10ba7a5a0488a9575b03351f7dff6cc
Sinh Dang <sinh.dang@stunited.vn>
2025-04-28 15:43:49 +0700

    fix: config

--- services/GenerateExcelTemplateService.js
+++ services/GenerateExcelTemplateService.js
@@ -49,13 +49,76 @@ function formatQuestionContent(questionContent) {
   return { leftItems, rightItems };
 }
 
+const formatQuestionToJson = (
+  question,
+  questionContent,
+  correctAnswer,
+  partID
+) => {
+  return {
+    title: question,
+    audioKey: "",
+    listContent: questionContent
+      .split("Options")
+      .slice(1)
+      .map((content, index) => {
+        const lines = content.split("\n").filter((line) => line.trim() !== "");
+        const questionText = lines[1].replace(/^\d+: /, "").trim();
+        const options = lines
+          .slice(2)
+          .map((opt) => opt.replace(/^[A-C]\)/, "").trim());
+
+        const correctOption = correctAnswer.match(
+          new RegExp(`Options ${index + 1}: (\\d+) \\| ([A-C])`)
+        );
+        const correctAnswerText = correctOption
+          ? options[parseInt(correctOption[1]) - 1]
+          : "";
+
+        return {
+          ID: index + 1,
+          content: questionText,
+          options: options,
+          correctAnswer: correctAnswerText,
+          type: "multiple-choice",
+          partID: partID,
+        };
+      }),
+  };
+};
+
+const formatAnswers = (answersStr) => {
+  return answersStr.split("\n").map((line) => {
+    const parts = line.split(".");
+    return parts.slice(1).join(".").trim();
+  });
+};
+
+const formatCorrectAnswer = (correctAnswerStr, questionContentStr) => {
+  const correctAnswers = correctAnswerStr.split("\n").map((line) => {
+    const [key, value] = line.split("|").map((s) => s.trim());
+    return { key, value: parseInt(value, 10) };
+  });
+
+  const options = questionContentStr.split("\n").map((line) => {
+    const [key, ...rest] = line.split(".");
+    const answer = rest.join(".").trim();
+    return { key: key.trim(), answer };
+  });
+
+  return correctAnswers.map((answer) => {
+    const matchingOption = options.find((option) => option.key === answer.key);
+    return { key: matchingOption.answer, value: answer.value };
+  });
+};
+
 const parseAnswers = (correctStr, contentStr) =>
   correctStr
     .trim()
     .split("\n")
     .map((line, i) => {
       const [keyPart, ansLetter] = line.split("|").map((s) => s.trim());
-      const key = keyPart.match(/\d+/)?.[0];
+      const key = keyPart.trim();
       const answer = ansLetter?.toUpperCase();
       const optionsLine =
         contentStr.split("\n")[i]?.split("|")[1]?.trim() || "";
@@ -148,123 +211,223 @@ const parseExcelBuffer = async (buffer) => {
     map[normalizeString(part.content)] = part.id;
     return map;
   }, {});
+  try {
+    for (const row of sheet.getRows(2, sheet.rowCount - 1) || []) {
+      const questionType = row
+        .getCell("E")
+        .value?.toString()
+        .toLowerCase()
+        .trim();
 
-  for (const row of sheet.getRows(2, sheet.rowCount - 1) || []) {
-    const questionType = row
-      .getCell("E")
-      .value?.toString()
-      .toLowerCase()
-      .trim();
+      const cells = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+      ].map((col) => row.getCell(col).value);
+      if (cells.every((v) => v === null || v === "")) break;
 
-    const cells = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-    ].map((col) => row.getCell(col).value);
-    if (cells.every((v) => v === null || v === "")) break;
+      const [
+        topic,
+        skillName,
+        partContent,
+        subPart,
+        type,
+        sequence,
+        audioLink,
+        imageLink,
+        question,
+        questionContent,
+        correctAnswer,
+        subQuestion,
+        groupQuestion,
+      ] = cells;
 
-    const [
-      topic,
-      skillName,
-      partContent,
-      subPart,
-      type,
-      sequence,
-      audioLink,
-      imageLink,
-      question,
-      questionContent,
-      correctAnswer,
-      subQuestion,
-      groupQuestion,
-    ] = cells;
+      console.log("====================================");
+      console.log(imageLink);
+      console.log("====================================");
+      const partID =
+        createdParts.find(
+          (s) =>
+            s.content.toLowerCase().replace(/\s+/g, "") ===
+            (partContent || "").toLowerCase().replace(/\s+/g, "")
+        )?.id || null;
 
-    const partID =
-      createdParts.find(
-        (s) =>
-          s.content.toLowerCase().replace(/\s+/g, "") ===
-          (partContent || "").toLowerCase().replace(/\s+/g, "")
-      )?.id || null;
+      const skillID =
+        skills.find(
+          (s) =>
+            s.Name.toLowerCase().replace(/\s+/g, "") ===
+            skillName.toLowerCase().replace(/\s+/g, "")
+        )?.ID || null;
 
-    const skillID =
-      skills.find(
-        (s) =>
-          s.Name.toLowerCase().replace(/\s+/g, "") ===
-          skillName.toLowerCase().replace(/\s+/g, "")
-      )?.ID || null;
+      if (type == "dropdown-list") {
+        const lines = (questionContent || "")
+          .split("\n")
+          .map((line) => line.trim())
+          .filter(Boolean);
+        const optionsAfterPipe = lines.map(
+          (line) => line.split("|")[1]?.trim().toLowerCase() || ""
+        );
+        const allSame = optionsAfterPipe.every(
+          (opt) => opt === optionsAfterPipe[0]
+        );
 
-    if (type == "dropdown-list") {
-      const lines = (questionContent || "")
-        .split("\n")
-        .map((line) => line.trim())
-        .filter(Boolean);
-      const optionsAfterPipe = lines.map(
-        (line) => line.split("|")[1]?.trim().toLowerCase() || ""
-      );
-      const allSame = optionsAfterPipe.every(
-        (opt) => opt === optionsAfterPipe[0]
-      );
-
-      if (!allSame) {
+        if (!allSame) {
+          await Question.create({
+            Type: type,
+            AudioKeys: audioLink ? audioLink.text : null,
+            ImageKeys: imageLink ? [imageLink] : null,
+            SkillID: skillID,
+            PartID: partID,
+            Sequence: sequence,
+            Content: question,
+            SubContent: subQuestion,
+            GroupContent: groupQuestion,
+            AnswerContent: {
+              content: question,
+              options: parseQuestionContent(questionContent),
+              correctAnswer: parseAnswers(correctAnswer, questionContent),
+              partID: partID,
+              type: questionType,
+              type: type,
+              ...(audioLink &&
+              typeof audioLink === "string" &&
+              audioLink.trim() !== ""
+                ? { audioKeys: audioLink }
+                : {}),
+            },
+          });
+        } else {
+          const { leftItems, rightItems } =
+            formatQuestionContent(questionContent);
+          await Question.create({
+            Type: type,
+            AudioKeys: audioLink ? audioLink.text : null,
+            ImageKeys: imageLink ? [imageLink] : null,
+            SkillID: skillID,
+            PartID: partID,
+            Sequence: sequence,
+            Content: question,
+            SubContent: subQuestion,
+            GroupContent: groupQuestion,
+            AnswerContent: {
+              content: question,
+              leftItems: leftItems,
+              rightItems: rightItems,
+              correctAnswer: parseAnswers(correctAnswer, questionContent),
+              partID: partID,
+              type: type,
+              ...(audioLink &&
+              typeof audioLink === "string" &&
+              audioLink.trim() !== ""
+                ? { audioKeys: audioLink.text }
+                : {}),
+            },
+          });
+        }
+      }
+      if (type == "listening-questions-group") {
         await Question.create({
           Type: type,
-          AudioKeys: audioLink,
-          ImageKeys: imageLink,
+          AudioKeys: audioLink ? audioLink.text : null,
+          ImageKeys: imageLink ? [imageLink] : null,
           SkillID: skillID,
           PartID: partID,
           Sequence: sequence,
           Content: question,
           SubContent: subQuestion,
-          GroupContent: groupQuestion,
+          GroupContent: formatQuestionToJson(
+            question,
+            questionContent,
+            correctAnswer,
+            partID
+          ),
           AnswerContent: {
             content: question,
-            options: parseQuestionContent(questionContent),
-            correctAnswer: parseAnswers(correctAnswer, questionContent),
-            partID: partID,
-            type: questionType,
-            type: type,
-            ...(audioLink && audioLink.trim() !== ""
-              ? { audioKeys: audioLink }
-              : {}),
-          },
-        });
-      } else {
-        const { leftItems, rightItems } =
-          formatQuestionContent(questionContent);
-        await Question.create({
-          Type: type,
-          AudioKeys: audioLink,
-          ImageKeys: imageLink,
-          SkillID: skillID,
-          PartID: partID,
-          Sequence: sequence,
-          Content: question,
-          SubContent: subQuestion,
-          GroupContent: groupQuestion,
-          AnswerContent: {
-            content: question,
-            leftItems: leftItems,
-            rightItems: rightItems,
-            correctAnswer: parseAnswers(correctAnswer, questionContent),
+            groupContent: formatQuestionToJson(
+              question,
+              questionContent,
+              correctAnswer,
+              partID
+            ),
             partID: partID,
             type: type,
-            ...(audioLink && audioLink.trim() !== ""
+            ...(audioLink &&
+            typeof audioLink === "string" &&
+            audioLink.trim() !== ""
               ? { audioKeys: audioLink }
               : {}),
           },
         });
       }
+      if (type == "ordering") {
+        await Question.create({
+          Type: type,
+          AudioKeys: audioLink ? audioLink.text : null,
+          ImageKeys: imageLink ? [imageLink] : null,
+          SkillID: skillID,
+          PartID: partID,
+          Sequence: sequence,
+          Content: question,
+          SubContent: subQuestion,
+          GroupContent: groupQuestion,
+          AnswerContent: {
+            content: question,
+            options: formatAnswers(questionContent),
+            correctAnswer: formatCorrectAnswer(correctAnswer, questionContent),
+            partID: partID,
+            type: type,
+          },
+        });
+      }
+      if (type == "speaking") {
+        await Question.create({
+          Type: type,
+          AudioKeys: audioLink ? audioLink.text : null,
+          ImageKeys: imageLink ? [imageLink] : null,
+          SkillID: skillID,
+          PartID: partID,
+          Sequence: sequence,
+          Content: question,
+          SubContent: subQuestion,
+          GroupContent: groupQuestion,
+          AnswerContent: {
+            content: question,
+            groupContent: groupQuestion,
+            options: questionContent,
+            correctAnswer: correctAnswer,
+            partID: partID,
+            type: type,
+            ImageKeys: `[${imageLink}]`,
+          },
+        });
+      }
+      if (type == "writing") {
+        await Question.create({
+          Type: type,
+          AudioKeys: audioLink ? audioLink : null,
+          ImageKeys: imageLink ? [imageLink] : null,
+          SkillID: skillID,
+          PartID: partID,
+          Sequence: sequence,
+          Content: question,
+          SubContent: subQuestion,
+          GroupContent: groupQuestion,
+          AnswerContent: correctAnswer,
+        });
+      }
     }
+  } catch (error) {
+    console.error("Error processing sheet rows:", error);
   }
 
   return { status: 200, message: "Parse Successfully" };
